651b9f20e661338fae05130f2055020bb9ec11c2
Josh Strange <josh@joshstrange.com>
2016-06-16 14:48:05 -0400

    Removing account id as plaid's docs must be incorrect: https://plaid.com/docs/link/stripe/#step-4-write-server-side-handler

--- src/Plaid.php
+++ src/Plaid.php
@@ -901,18 +901,16 @@ class Plaid
      * [exchangeToken description]
      * @method exchangeToken
      * @param [string]  $public_token Token returned from plaid link
-     * @param [string]  $account_id   Account Id returned from plaid link
      * @return [type] [description]
      */
-    public static function exchangeToken($public_token, $account_id)
+    public static function exchangeToken($public_token)
     {
         try {
             $request = self::client()->post('exchange_token', [
                 'body' => [
                     'client_id' => config('plaid.client_id'),
                     'secret' => config('plaid.secret'),
-                    'public_token' => $public_token,
-                    'account_id' => $account_id
+                    'public_token' => $public_token
                 ]
             ]);
             return $request->json();
