382c1e758c755834d75e5d9ea0e2a86387e7fb72
Ben Warburton <ben@travoltron.com>
2018-08-10 11:46:32 -0400

    Overzealous logging

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -25,7 +25,6 @@ class AuthController extends BaseController
             return $this->needsMfa($auth);
         }
         if($this->hasError($auth)) {
-            \Log::error(\App\Models\User::uuid($request->header('uuid'))->name());
             \Log::error($this->hasError($auth));
             return $this->hasError($auth);
         }
